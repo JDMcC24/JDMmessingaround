3e261a9e6fe7909edf018a5ae11c5b125834aced
JDMcC24 <mccaslij@outlook.com>
2024-10-30 12:18:04 -0700

    Update ML_first_project.py

--- Hands_On_ML/Chapter 2/Chapter_2/ML_first_project.py
+++ Hands_On_ML/Chapter 2/Chapter_2/ML_first_project.py
@@ -59,7 +59,7 @@ housing.plot(kind="scatter", x="longitude", y="latitude", grid=True,
 #plt.show()
 
 corr_matrix = housing.corr(numeric_only=True)
-print(corr_matrix["median_house_value"].sort_values(ascending=False))
+#print(corr_matrix["median_house_value"].sort_values(ascending=False))
 
 from pandas.plotting import scatter_matrix
 attributes = ["median_house_value", "median_income", "total_rooms", "housing_median_age"]
@@ -151,11 +151,10 @@ from sklearn.preprocessing import FunctionTransformer
 log_transformer = FunctionTransformer(np.log, inverse_func=np.exp)
 log_pop = log_transformer.transform(housing[["population"]])
 
-# from sklearn.metrics.pairwise import rbf_kernel
+from sklearn.metrics.pairwise import rbf_kernel
 
-# rbf_transformer = FunctionTransformer(rbf_kernel,
-#                                       kw_args=dict(Y=[[35.]], gamma = 0.1))
-# age_sim_35 = rbf_transformer(housing[["housing_median_age"]])
+age_simil_35 = rbf_kernel(housing[["housing_median_age"]], [[35]], gamma=0.1)
+
 
 from sklearn.pipeline import Pipeline
 num_pipeline = Pipeline([ ("impute", SimpleImputer(strategy="median")),
@@ -166,12 +165,12 @@ from sklearn.pipeline import make_pipeline
 num_pipeline = make_pipeline(SimpleImputer(strategy="median"), StandardScaler())
 
 housing_num_prepared = num_pipeline.fit_transform(housing_num)
-print(housing_num_prepared[:2].round(2))
+#print(housing_num_prepared[:2].round(2))
 df_housing_num_prepared = pd.DataFrame(
     housing_num_prepared, columns=num_pipeline.get_feature_names_out(),
     index = housing_num.index
 )
-print(df_housing_num_prepared.head())
+#print(df_housing_num_prepared.head())
 
 from sklearn.compose import ColumnTransformer
 num_attribs = ["longitude", "latitude", "housing_median_age", "total_rooms", "total_bedrooms", "population", "households",
@@ -183,10 +182,10 @@ cat_pipeline = make_pipeline(
     OneHotEncoder(handle_unknown="ignore")
 )
 
-# preprocessing = ColumnTransformer([
-#     ("num", num_pipeline, num_attribs),
-#     ("cat", cat_pipeline, cat_attribs)
-# ])
+preprocessing = ColumnTransformer([
+    ("num", num_pipeline, num_attribs),
+    ("cat", cat_pipeline, cat_attribs)
+])
 
 from sklearn.compose import make_column_selector, make_column_transformer
 
@@ -196,3 +195,133 @@ preprocessing = make_column_transformer(
 )
 
 housing_prepared = preprocessing.fit_transform(housing)
+
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_array, check_is_fitted
+
+class StandardScalerClone(BaseEstimator, TransformerMixin):
+    def __init__(self, with_mean=True):  # no *args or **kwargs!
+        self.with_mean = with_mean
+
+    def fit(self, X, y=None):  # y is required even though we don't use it
+        X = check_array(X)  # checks that X is an array with finite float values
+        self.mean_ = X.mean(axis=0)
+        self.scale_ = X.std(axis=0)
+        self.n_features_in_ = X.shape[1]  # every estimator stores this in fit()
+        return self  # always return self!
+
+    def transform(self, X):
+        check_is_fitted(self)  # looks for learned attributes (with trailing _)
+        X = check_array(X)
+        assert self.n_features_in_ == X.shape[1]
+        if self.with_mean:
+            X = X - self.mean_
+        return X / self.scale_
+
+from sklearn.cluster import KMeans
+
+class ClusterSimilarity(BaseEstimator, TransformerMixin):
+    def __init__(self, n_clusters=10, gamma=1.0, random_state=None):
+        self.n_clusters = n_clusters
+        self.gamma = gamma
+        self.random_state = random_state
+
+    def fit(self, X, y=None, sample_weight=None):
+        self.kmeans_ = KMeans(self.n_clusters, n_init=10,
+                              random_state=self.random_state)
+        self.kmeans_.fit(X, sample_weight=sample_weight)
+        return self  # always return self!
+
+    def transform(self, X):
+        return rbf_kernel(X, self.kmeans_.cluster_centers_, gamma=self.gamma)
+    
+    def get_feature_names_out(self, names=None):
+        return [f"Cluster {i} similarity" for i in range(self.n_clusters)]
+
+
+def column_ratio(X):
+    return X[:, [0]]/X[:,[1]]
+
+def ratio_name(function_transformer, feature_names_in):
+    return ["ratio"]
+
+def ratio_pipeline():
+    return make_pipeline(
+        SimpleImputer(strategy= "median"),
+        FunctionTransformer(column_ratio, feature_names_out= ratio_name),
+        StandardScaler())
+
+log_pipeline = make_pipeline(
+    SimpleImputer(strategy="median"),
+    FunctionTransformer(np.log, feature_names_out= "one-to-one"),
+    StandardScaler())
+cluster_simil = ClusterSimilarity(n_clusters= 10, gamma= 1., random_state= 42)
+default_num_pipeline = make_pipeline(SimpleImputer(strategy="median"), StandardScaler())
+
+preprocessing = ColumnTransformer([
+    ("bedrooms", ratio_pipeline(), ["total_bedrooms", "total_rooms"]), 
+    ("rooms_per_house", ratio_pipeline(), ["total_rooms", "households"]),
+    ("people_per_house", ratio_pipeline(), ["population", "households"]),
+    ("log", log_pipeline, ["total_bedrooms", "total_rooms", "population", "households", "median_income"]),
+    ("geo", cluster_simil, ["latitude", "longitude"]),
+    ("cat", cat_pipeline, make_column_selector(dtype_include= object))],
+    remainder= default_num_pipeline 
+    )
+
+housing_prepared = preprocessing.fit_transform(housing)
+#print(housing_prepared.shape)
+
+from sklearn.linear_model import LinearRegression
+lin_reg = make_pipeline(preprocessing, LinearRegression())
+lin_reg.fit(housing, housing_labels)
+
+housing_predictions = lin_reg.predict(housing)
+# print(housing_predictions[:5].round(-2))
+# print(housing_labels.iloc[:5].values)
+
+from sklearn.metrics import mean_squared_error
+#lin_rmse = mean_squared_error(housing_labels, housing_predictions, squared =False)
+#print(lin_rmse)
+
+# from sklearn.tree import DecisionTreeRegressor
+
+# tree_reg = make_pipeline(preprocessing, DecisionTreeRegressor(random_state=42))
+# tree_reg.fit(housing, housing_labels)
+# housing_predictions = tree_reg.predict(housing)
+# tree_rmse = mean_squared_error(housing_labels, housing_predictions, squared= False)
+# #print(tree_rmse)
+
+from sklearn.model_selection import cross_val_score
+# tree_rmses = -cross_val_score(tree_reg, housing, housing_labels, scoring = "neg_root_mean_squared_error", cv = 10)
+# #print(pd.Series(tree_rmses).describe())
+
+from sklearn.ensemble import RandomForestRegressor
+
+forest_reg = make_pipeline(preprocessing,
+                           RandomForestRegressor(random_state=42))
+# forest_rmses = -cross_val_score(forest_reg, housing, housing_labels,
+#                                 scoring="neg_root_mean_squared_error", cv=10)
+# print(pd.Series(forest_rmses).describe())
+# forest_reg.fit(housing, housing_labels)
+# housing_predictions = forest_reg.predict(housing)
+# forest_rmse = mean_squared_error(housing_labels, housing_predictions, squared= False)
+# print(forest_rmse)
+
+from sklearn.model_selection import GridSearchCV
+
+full_pipeline = Pipeline([
+    ("preprocessing", preprocessing),
+    ("random_forest", RandomForestRegressor(random_state=42)),
+])
+param_grid = [
+    {'preprocessing__geo__n_clusters': [5, 8, 10],
+     'random_forest__max_features': [4, 6, 8]},
+    {'preprocessing__geo__n_clusters': [10, 15],
+     'random_forest__max_features': [6, 8, 10]},]
+grid_search = GridSearchCV(full_pipeline, param_grid, cv=3,
+                           scoring='neg_root_mean_squared_error')
+grid_search.fit(housing, housing_labels)
+print(grid_search.best_params_)
+cv_res = pd.DataFrame(grid_search.cv_results_)
+cv_res.sort_values( by = "mean_test_score", ascending= False, inplace= True)
+print(cv_res.head())
