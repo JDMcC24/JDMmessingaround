e854dc11ba3b58af1a18040382e00fc8e5a58717
JDMcC24 <mccaslij@outlook.com>
2024-11-13 10:32:02 -0800

    pe_problem69 done

--- ProjectEuler/pe_problem69.py
+++ ProjectEuler/pe_problem69.py
@@ -7,16 +7,23 @@ def relative_totient(n):
 
 
 starttime = time.time()
-limit = 10**6
-maxratio = 1
-for n in range(2, limit):
-    m = relative_totient(n)
-    if m > maxratio:
-        maxratio = m
-        answr =n
-print(answr, maxratio)
+# limit = 7*10**3
+# maxratio = 1
+# for n in range(2, limit):
+#     m = relative_totient(n)
+#     if m > maxratio:
+#         maxratio = m
+#         answr =n
+# print(answr, maxratio)
+
+last_prime = 2
+max=2
+while max < 10**6:
+    p = next_prime(last_prime)
+    max *= p
+    last_prime = p
+print(max/prime_factors(max)[-1])
+
 print(time.time() - starttime)
-
-
 
 
